89697d1f847deda4824af8980385328c81423f2e
Rafi A <Sleekfish8@gmail.com>
2026-04-22 08:21:52 +0700

    ok

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -1,14 +1,23 @@
 package domain
 
-import (
-	"gorm.io/gorm"
-)
+import "time"
 
 type User struct {
-	gorm.Model
-	ID       uint
-	Username string
-	Email    string
-	Password string
-	Role     int
+	ID        uint
+	Username  string
+	Email     string
+	Password  string
+	Role      string
+	CreatedAt time.Time
+}
+
+type UserRepository interface {
+	FindByID(id uint) (*User, error)
+	FindByEmail(email string) (*User, error)
+	Create(user *User) error
+}
+
+type UserService interface {
+	UserRegister(user *User) error
+	UserLogin(email, password string) (string, error)
 }
