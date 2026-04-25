2b3951fbfb92fd3882ac4f4caf43a8ea07e0aa18
restartor <sleekfish8@gmail.com>
2026-04-25 19:49:32 +0700

    add database connection

--- config/database.go
+++ config/database.go
@@ -1,1 +1,43 @@
 package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"web-streaming/internal/domain"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func DatabaseConnection() {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_SSLMODE"))
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("gagal koneksi database", err)
+	}
+
+	err = db.AutoMigrate(
+		&domain.User{},
+		&domain.Filem{},
+	)
+
+	if err != nil {
+		log.Fatal("migrate gagal", err)
+	}
+
+	DB = db
+
+	fmt.Println("Koneksi ke database success!")
+
+}
