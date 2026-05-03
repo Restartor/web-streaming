280b79e108c888e219f192db406cd4a7756ae774
restartor <sleekfish8@gmail.com>
2026-05-03 22:33:05 +0700

    add response for frontend but not update all of the message in the handler

---
+++ backend/pkg/response/response.go
@@ -0,0 +1,16 @@
+package response
+
+import "github.com/gin-gonic/gin"
+
+type DataResponse struct {
+	Data  interface{} `json:"data"`
+	Error interface{} `json:"error"`
+}
+
+func Success(c *gin.Context, status int, data interface{}) {
+	c.JSON(status, DataResponse{Data: data, Error: nil})
+}
+
+func Error(c *gin.Context, status int, err string) {
+	c.JSON(status, DataResponse{Data: nil, Error: err})
+}
--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -13,7 +13,7 @@ func SetupRoutes(
 	userHandler *handler.UserHandler,
 	filmHandler *handler.FilmHandler,
 ) {
-	user := routes.Group("/api")
+	user := routes.Group("/api/v1")
 	{
 		user.POST("/register", middleware.RateLimiter("5-M"), userHandler.Register)
 		user.POST("/login", middleware.RateLimiter("10-M"), userHandler.Login)
@@ -21,7 +21,7 @@ func SetupRoutes(
 		user.GET("/films/search", filmHandler.GetFilmByTitle)
 	}
 
-	protected := routes.Group("/api")
+	protected := routes.Group("/api/v1")
 	protected.Use(middleware.AuthMiddleware(), pkg.AdminOnly())
 	{
 		protected.POST("/films", filmHandler.CreateFilm)
